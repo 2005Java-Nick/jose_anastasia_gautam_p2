c5965c1ed42ab3e96bef7187e51cf47edccc8d1e
Gautam <gramasu000@citymail.cuny.edu>
2020-06-10 19:11:02 -0400

    Added an assignment-instance-display interface

--- AngularAMS/src/app/component/teacher-dashboard/teacher-dashboard.component.ts
+++ AngularAMS/src/app/component/teacher-dashboard/teacher-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AssignmentInstanceDisplay } from '../../interface/assignmentinstancedisplay.interface';
 
 @Component({
   selector: 'app-teacher-dashboard',
@@ -23,16 +24,24 @@ export class TeacherDashboardComponent implements OnInit {
     }
   ];
 
-  listOfAssignments = [
+  listOfAssignments : AssignmentInstanceDisplay[] = [
     {
-      assignmentName: "History",
-      studentName: "Jane Doe"
-    },
-    {
-      assignmentName: "Math",
-      studentName: "John Doe"
+      course: "History",
+      assignmentType: "Exam",
+      assignmentStatus: "Completed",
+      assignmentName: "US History (1600s-1776)",
+      teacherName: "Mrs. Jane Doe",
+      studentName: "John Doe",
+      dueDate: {
+        date: "06-10-2020",
+        time: "5:00 PM"
+      },
+      completionDate: {
+        date: "06-10-2020",
+        time: "4:45 PM"
+      },
+      grade: null
     }
-
   ];
 
 
---
+++ AngularAMS/src/app/interface/assignmentinstancedisplay.interface.ts
@@ -0,0 +1,17 @@
+export interface AssignmentInstanceDisplay {
+  course : string;
+  assignmentType: string;
+  assignmentStatus: string;
+  assignmentName: string;
+  teacherName: string;
+  studentName: string;
+  dueDate: {
+    date: string;
+    time: string;
+  }
+  completionDate: {
+    date: string;
+    time: string;
+  }
+  grade: number;
+}
