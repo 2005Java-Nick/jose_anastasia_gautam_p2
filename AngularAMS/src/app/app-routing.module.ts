f13d51660d6e415bd90e940e4d7db18042b90335
Gautam <gramasu000@citymail.cuny.edu>
2020-06-10 18:50:41 -0400

    Added basic routing to teacher and student dashboards

--- AngularAMS/src/app/app-routing.module.ts
+++ AngularAMS/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeacherDashboardComponent } from './component/teacher-dashboard/teacher-dashboard.component';
+import { StudentDashboardComponent } from './component/student-dashboard/student-dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "teacher-dashboard", component: TeacherDashboardComponent },
+  { path: "student-dashboard", component: StudentDashboardComponent },
+  { path: "", redirectTo: "/student-dashboard", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularAMS/src/app/interface/assignmentinstance.interface.ts
+++ AngularAMS/src/app/interface/assignmentinstance.interface.ts
@@ -4,6 +4,7 @@ assignmentInstanceID: number;
 assignmentTemplateID: number;
 teacherID: number;
 studentID: number;
+assignmentStatus: string;
 completionDateTime: string;
 gradedDateTime: string;
 finalGrade: number;
