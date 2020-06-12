1cdb43a4bde53de17602bde20a1892a19a46f023
Anastasia Miagkii <anastasiamiagkii@gmail.com>
2020-06-12 12:10:38 -0400

    register

--- AngularAMS/src/app/component/register/register.component.ts
+++ AngularAMS/src/app/component/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
 import { SidebarOptionsService } from '../../service/sidebar-options.service';
 
 @Component({
@@ -8,11 +9,27 @@ import { SidebarOptionsService } from '../../service/sidebar-options.service';
 })
 export class RegisterComponent implements OnInit {
 
-  sidebarOptions;
+  registerForm: FormGroup = this.fb.group({
+    userId: [''],
+    userFirstName: [''],
+    userLastName: [''],
+    userEmail: [''],
+    userPassword: [''],
+    userClass: ['']
+});
 
-  constructor(sidebarOptionsService : SidebarOptionsService) {
-    this.sidebarOptions = sidebarOptionsService.loginRegisterSideOptions("/register");
-  }
+sidebarOptions;
+
+headerTitle: string = "Welcome";
+
+constructor(private sidebarOptionsService : SidebarOptionsService, private fb: FormBuilder) {
+  this.sidebarOptions = sidebarOptionsService.loginRegisterSideOptions('/register');
+}
+
+submit() {
+  console.log(this.registerForm.value);
+}
+
   ngOnInit(): void {
   }
 
