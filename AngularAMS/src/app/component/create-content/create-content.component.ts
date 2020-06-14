586759a7e195c6010ed1412a02e0501054c40948
Gautam <gramasu000@citymail.cuny.edu>
2020-06-14 13:17:21 -0400

    Completed a decent version of create Assignment

--- AngularAMS/src/app/component/create-content/create-content.component.ts
+++ AngularAMS/src/app/component/create-content/create-content.component.ts
@@ -8,13 +8,14 @@ import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
 })
 export class CreateContentComponent implements OnInit {
 
+  questions : number[] = [0];
+
   createAssignmentForm : FormGroup = this.fb.group({
     assignmentType: [''],
     assignmentTitle: [''],
     assignTo: [''],
-    questions: {
-      0: [''] 
-    }
+    question0: [''],
+    point0: ['']
   });
 
   constructor(private fb : FormBuilder) { }
@@ -22,8 +23,35 @@ export class CreateContentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addQuestion() {
+    let newQuestion;
+    if (this.questions.length === 0) {
+      newQuestion = 0;
+    } else {
+      newQuestion = this.questions[this.questions.length - 1] + 1;
+    }
+    let label = "question" + newQuestion;
+    let formControl = new FormControl('');
+    this.createAssignmentForm.addControl(label, formControl);
+    label = "point" + newQuestion;
+    formControl = new FormControl('');
+    this.createAssignmentForm.addControl(label, formControl);
+    this.questions.push(newQuestion);
+  }
+
+  removeQuestion(i : number) {
+    let index = this.questions.indexOf(i);
+    this.questions.splice(index, 1);
+    this.createAssignmentForm.removeControl("question" + i);
+    this.createAssignmentForm.removeControl("point" + i);
+  }
+
   onSubmit() {
-    console.log(this.createAssignmentForm.value);
+    let submitObject = {
+      questions: this.questions,
+      form: this.createAssignmentForm.value
+    }
+    console.log(submitObject);
   }
 
 }
