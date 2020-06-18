66375e477410374985491e17abd36cf35cc8a701
Gautam <gramasu000@citymail.cuny.edu>
2020-06-18 14:23:16 -0400

    Imported Cross Origin

--- JavaAMS/src/main/java/com/revature/ams/controller/LoginController.java
+++ JavaAMS/src/main/java/com/revature/ams/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.revature.ams.dto.Message;
 import com.revature.ams.services.LoginService;
