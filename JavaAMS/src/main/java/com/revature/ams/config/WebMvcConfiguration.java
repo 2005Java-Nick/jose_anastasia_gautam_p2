b9623ab80cb1b836ea3872a7e9c557b0bff0e983
Jose Canela <jose.canela916@gmail.com>
2020-06-18 13:29:00 -0400

    Modified WebMvcConfiguration

--- JavaAMS/src/main/java/com/revature/ams/config/WebMvcConfiguration.java
+++ JavaAMS/src/main/java/com/revature/ams/config/WebMvcConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @ComponentScan(basePackages = "com.revature.ams")
 @EnableWebMvc
+@CrossOrigin
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
   @Override
