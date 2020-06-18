7a783d2171fc514d6df91fb0be9ad3a271df791d
Gautam <gramasu000@citymail.cuny.edu>
2020-06-18 10:02:19 -0400

    Added MVC configuration for resources

--- JavaAMS/src/main/java/com/revature/ams/config/WebMvcConfiguration.java
+++ JavaAMS/src/main/java/com/revature/ams/config/WebMvcConfiguration.java
@@ -6,13 +6,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @ComponentScan(basePackages = "com.revature.ams")
 @EnableWebMvc
-public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+public class WebMvcConfiguration extends WebMvcConfigurer {
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/resources/**");
+    registry.addResourceHandler("/resources/");
+  }
 
 }
