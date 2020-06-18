2af20919669dfc43f9b9434cf34f547d747244a0
Jose Canela <jose.canela916@gmail.com>
2020-06-18 17:12:32 -0400

    Didnt fix bug

--- JavaAMS/src/main/java/com/revature/ams/controller/GetNewAssignmentsController.java
+++ JavaAMS/src/main/java/com/revature/ams/controller/GetNewAssignmentsController.java
@@ -40,13 +40,13 @@ public class GetNewAssignmentsController {
 	@RequestMapping(path = "/student-new-assignments", method = RequestMethod.GET)
 	@ResponseBody
 	@CrossOrigin
-	public List<AssignmentInstanceDTO> getNewAssignments(@RequestParam(name = "studentId",required = true) String studentId, @RequestParam(name = "token",required = true) String token){
-		System.out.println("GAUTAM HERE IS THE LIST!!!!!!!: "+aiService.getNewAssignmentInstancesByStudent(Integer.valueOf(studentId)));
+	public List<AssignmentInstanceDTO> getNewAssignments(@RequestParam(name = "studentId",required = true)Integer studentId, @RequestParam(name = "token",required = true) String token){
+		System.out.println("GAUTAM HERE IS THE LIST!!!!!!!: "+aiService.getNewAssignmentInstancesByStudent(studentId));
 		
 		if(aService.authorizeStudent(Integer.valueOf(studentId), token)) {
 			System.out.println("GAUTAM HERE IS THE LIST!!!!!!!: "+aiService.getNewAssignmentInstancesByStudent(123456789));
 			
-			return aiService.getNewAssignmentInstancesByStudent(Integer.valueOf(studentId));
+			return aiService.getNewAssignmentInstancesByStudent(studentId);
 		}else {
 			return null;
 		}
